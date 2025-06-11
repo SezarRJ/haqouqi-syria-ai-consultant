dc97d0e87c8e9e6f4f81804ff24383c6b3f338f0
SezarRJ <Rafi6630@gmail.com>
2025-06-12 01:45:34 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { Toaster } from '@/components/ui/toaster';
